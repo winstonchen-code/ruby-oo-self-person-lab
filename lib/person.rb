12ca80dcdd3c371966ea41f2c2ca7bdaa6fc1a8f
Blaire Baker <bakerblaire@gmail.com>
2020-11-18 15:38:42 -0600

    passed up to hygiene

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,29 @@
-# your code goes here
+class Person
+    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness
+        if @happiness > 10
+            @happiness = 10
+        elsif @happiness < 0
+            @happiness = 0
+        end
+        @happiness
+    end
+
+    def hygiene
+        if @hygiene > 10
+            @hygiene = 10
+        elsif @hygiene < 0
+            @hygiene = 0
+        end
+        @hygiene
+    end
+end
